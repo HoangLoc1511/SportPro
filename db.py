6466751017728cea79b4bd627600499e51f4279c
HoangLoc1511 <truonghoanglock4@gmail.com>
2025-05-26 05:08:18 +0700

    Update db.py

--- db.py
+++ db.py
@@ -4,12 +4,13 @@ import pyodbc
 def get_connection():
     server = os.getenv('DB_SERVER', 'ADMIN-PC')   # Tên máy chủ SQL Server hoặc IP
     database = os.getenv('DB_NAME', 'sportpro')   # Tên database
-
+    username = os.getenv('DB_USER', 'Admin') 
     driver = '{ODBC Driver 17 for SQL Server}'
 
     conn_str = (
         f'DRIVER={driver};'
         f'SERVER={server};'
+        f'UID={username};'
         f'DATABASE={database};'
         'Trusted_Connection=yes;'
         'TrustServerCertificate=yes;'
