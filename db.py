aa5a91615735f6adcddb9a941c061cf0a13a8a53
HoangLoc1511 <truonghoanglock4@gmail.com>
2025-05-26 06:09:51 +0700

    Update db.py

--- db.py
+++ db.py
@@ -3,16 +3,25 @@ import mysql.connector
 from mysql.connector import Error
 
 def get_connection():
+    server = os.getenv('DB_SERVER', '118.68.201.19')  # Public IP của bạn
+    database = os.getenv('DB_NAME', 'sportpro')
+    username = os.getenv('DB_USER', 'sa')
+    password = os.getenv('DB_PASSWORD', '123456')
+    driver = '{ODBC Driver 17 for SQL Server}'
+
+    conn_str = (
+        f'DRIVER={driver};'
+        f'SERVER={server},1433;'
+        f'DATABASE={database};'
+        f'UID={username};'
+        f'PWD={password};'
+        'TrustServerCertificate=yes;'
+    )
+
     try:
-        connection = mysql.connector.connect(
-            host=os.getenv('DB_SERVER', '118.68.201.19'),  # hoặc IP Public
-            port=int(os.getenv('DB_PORT', 3306)),
-            database=os.getenv('DB_NAME', 'sportpro'),
-            user=os.getenv('DB_USER', 'sa'),
-            password=os.getenv('DB_PASSWORD', '123456')
-        )
-        if connection.is_connected():
-            return connection
-    except Error as e:
-        print(f"Lỗi kết nối MySQL: {e}")
+        connection = pyodbc.connect(conn_str)
+        return connection
+    except pyodbc.Error as e:
+        print(f"Lỗi kết nối CSDL: {e}")
         raise
+
