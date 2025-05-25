6f566f8779a089bfd39f5a9cbbb423cfb9c206cb
HoangLoc1511 <truonghoanglock4@gmail.com>
2025-05-26 06:08:19 +0700

    Update db.py

--- db.py
+++ db.py
@@ -5,8 +5,8 @@ from mysql.connector import Error
 def get_connection():
     try:
         connection = mysql.connector.connect(
-            host=os.getenv('DB_SERVER', 'ADMIN-PC'),  # hoặc IP Public
-            port=int(os.getenv('DB_PORT', 3306, 10000)),
+            host=os.getenv('DB_SERVER', '118.68.201.19'),  # hoặc IP Public
+            port=int(os.getenv('DB_PORT', 3306)),
             database=os.getenv('DB_NAME', 'sportpro'),
             user=os.getenv('DB_USER', 'sa'),
             password=os.getenv('DB_PASSWORD', '123456')
