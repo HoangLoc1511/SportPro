5a2e03787aae55e174b9138e0a1015b7f7bbbd57
HoangLoc1511 <truonghoanglock4@gmail.com>
2025-05-26 07:52:53 +0700

    Update db.py

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ import os
 import pyodbc
 
 def get_connection():
-    server = os.getenv('DB_SERVER', '118.68.201.19')  # Địa chỉ IP SQL Server của bạn
+    server = os.getenv('DB_SERVER', '192.168.0.106')  # Địa chỉ IP SQL Server của bạn
     database = os.getenv('DB_NAME', 'sportpro')
     username = os.getenv('DB_USER', 'sa')
     password = os.getenv('DB_PASSWORD', '123456')
